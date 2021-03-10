de2a529fa8594d8f8dc660aa59d91b115f05703e
SilMontes <Silmontes2001@gmail.com>
2021-03-10 22:26:19 +0000

    Consumiendo una API

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 export function Home() {
+	const [equipo, setEquipo] = useState([]);
+	useEffect(() => {
+		//console.log("Hola");
+		//para ejecutar el obtenerDatos tenemos que llamarlo
+		obtenerDatos();
+	}, []);
+	//agregamos los corchetes para que el useEffect se realice solo una vez
+
+	//usaremos API  Esta función podríamos usarla dentro de useEffect
+	const obtenerDatos = async () => {
+		//el fecth nos ayuda a acceder a la API. Esto nos devuelve la data
+		const data = await fetch("https://jsonplaceholder.typicode.com/users");
+		//trasformaremos la data a JSON
+		const users = await data.json();
+		console.log(users);
+		setEquipo(users);
+	};
+
 	return (
 		<div className="text-center mt-5">
 			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			{/* {<p>{JSON.stringify(equipo)}</p>} */}
+			<ul>
+				{equipo.map(item => (
+					<li key={item.id}>
+						{item.name} - {item.email}
+					</li>
+				))}
+			</ul>
 		</div>
 	);
 }
