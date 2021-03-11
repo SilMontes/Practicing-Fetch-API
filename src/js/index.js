0a9516c6bcbcfe686eff22405dee853ad5a7ec5a
SilMontes <Silmontes2001@gmail.com>
2021-03-11 06:21:42 +0000

    Todolist

--- src/js/index.js
+++ src/js/index.js
@@ -11,9 +11,11 @@ import "../styles/index.scss";
 //import your own components
 //import { Home } from "./component/home.js";
 //import { RutasConParametrosApi } from "./component/ruta-parametros-api";
-import { ArrayMapping } from "./component/mappingarrays";
+//import { ArrayMapping } from "./component/mappingarrays";
+import { Todos } from "./component/todolist-breathe";
 
 //render your react application
 //ReactDOM.render(<Home />, document.querySelector("#app"));
 //ReactDOM.render(<RutasConParametrosApi />, document.querySelector("#app"));
-ReactDOM.render(<ArrayMapping />, document.querySelector("#app"));
+//ReactDOM.render(<ArrayMapping />, document.querySelector("#app"));
+ReactDOM.render(<Todos />, document.querySelector("#app"));
